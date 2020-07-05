487fdfd90d542407e37c1c3f4d96648731dc66a5
unknown <ibrahim@unico.aero>
2020-07-05 23:48:47 +0300

    first commit

---
+++ basicCondition.py
@@ -0,0 +1,7 @@
+username = "Jokerinya"
+password = "qQ?*)=istrp.,"
+entered_name = input("Hello!, please enter your name: ")
+if username == entered_name:
+    print(f"Hello, {entered_name}! The password is : {password}")
+else:
+    print(f"Hello, {entered_name}! See you later.")
