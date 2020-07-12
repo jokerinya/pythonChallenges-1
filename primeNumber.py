9fb8fed620d4ad9a1c41a0561361032863a33c65
Jokerinya <ibrahim@unico.aero>
2020-07-12 20:05:45 +0300

    logic editted

--- primeNumber.py
+++ primeNumber.py
@@ -9,18 +9,17 @@ input →  10 ⇉ output : 10 is not a prime number
 
 """
 number = input("Please enter an integer number: ").strip()
-check_numbers = [1, 2, 3, 5, 7]
+check_numbers = [2, 3, 5, 7]
 control_list = []
 if(number.isdigit()):
     number = int(number)
     if number in check_numbers:
         print(f"Your number {number} is a Prime Number.")
+    elif number == 1:
+        print(f"Your number {number} is NOT a Prime Number.")
     else:
         for i in check_numbers:
-            if(i == 1):
-                continue
-            else:
-                control_list.append(number % i)
+            control_list.append(number % i)
         if False in control_list:
             print(f"Your number {number} is NOT a Prime Number.")
         else:
