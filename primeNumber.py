c9b280c12fc6804997449536268781dc9cb1711f
Jokerinya <sakacibrahim@gmail.com>
2021-03-16 21:59:59 +0200

    prime number challenge editted

--- primeNumber.py
+++ primeNumber.py
@@ -9,19 +9,18 @@ input →  10 ⇉ output : 10 is not a prime number
 
 """
 number = input("Please enter an integer number: ").strip()
-check_numbers = [2, 3, 5, 7]
-control_list = []
+
 if(number.isdigit()):
     number = int(number)
-    if number in check_numbers:
-        print(f"Your number {number} is a Prime Number.")
-    elif number == 1:
+    if(number<=1):
+        print(f"Your number {number} is NOT a Prime Number.")
+    elif(number%2 == 0):
         print(f"Your number {number} is NOT a Prime Number.")
     else:
-        for i in check_numbers:
-            control_list.append(number % i)
-        if False in control_list:
-            print(f"Your number {number} is NOT a Prime Number.")
+        for i in range(2, number//2 + 1):
+            if(number%i==0):
+                print(f"Your number {number} is NOT a Prime Number.")
+                break
         else:
             print(f"Your number {number} is a Prime Number.")
 else:
